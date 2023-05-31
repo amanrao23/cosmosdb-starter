3fae34e5557a889f08b2846e418c58e5870867e0
Aman Rao <amanrao18613@gmail.com>
2023-06-01 00:35:51 +0530

    add try block

--- pages/api/todos/create.js
+++ pages/api/todos/create.js
@@ -6,8 +6,12 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { title } = req.body;
     const newTodo = { id: uuidv4(), title, completed: false };
+    try{
     const { resource: createdTodo } = await container.items.create(newTodo);
     res.status(201).json(createdTodo);
+    }catch(err){
+        console.log(err);
+    }
   } else {
     res.status(405).end(); // Method Not Allowed
   }
